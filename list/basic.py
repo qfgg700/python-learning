6d12bdd125b23e6eb23d8ca069bdb6cc5a35bd8e
qfgg700 <qfgg700@gmail.com>
2021-03-27 22:12:03 +0800

    list-1

---
+++ list/basic.py
@@ -0,0 +1,6 @@
+name_list = ['jimmy', 'tom', 'jerry']
+
+print(name_list)
+print(name_list[0])
+print(name_list[1])
+print(name_list[2])
---
+++ list/index.py
@@ -0,0 +1,10 @@
+name_list = ['jimmy', 'tom', 'jerry']
+
+# index
+print(name_list.index('tom', 0, 2))
+
+# count
+print(name_list.count('jimmy'))
+
+# len
+print(len(name_list))
